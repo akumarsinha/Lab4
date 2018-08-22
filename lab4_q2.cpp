69e65e08523b64b033b7ae75c3c824a8633a5713
akumarsinha <42178849+akumarsinha@users.noreply.github.com>
2018-08-22 00:55:35 -0400

    Add files via upload

---
+++ lab4_q2.cpp
@@ -0,0 +1,15 @@
+//library
+#include <iostream>
+using namespace std;
+//entering main function
+int main()
+{
+          //declaring variables
+          float fahr,cel;
+          //process
+          cout<< "enter temperature in celsius";
+          cin>> cel;
+          fahr = (((*CEL)?5)+32);
+          cout<< "entered temperature in fahrenheit unit is " <<fahr<<endl;
+          return 0;
+}
